b049f23dd5f1e0950cc639e90daefbe60b6dc313
Aloys Baillet <aloys.baillet@gmail.com>
2016-01-13 22:23:42 +1100

    Not using react-select anymore so looks better with Bootstrap

--- web/src/coffeeorder.js
+++ web/src/coffeeorder.js
@@ -1,109 +1,69 @@
 var React = require('react');
-var Select = require('react-select');
+var ReactFireMixin = require('reactfire');
 var Button = require('react-bootstrap').Button;
 
 var CoffeeOrder = React.createClass({
+  mixins: [ReactFireMixin],
 
   getInitialState: function() {
     return {
       coffeeType:'',
-      coffeeTypeOptions: [],
+      coffeeTypeList: [],
       sugar:0,
-      sugarOptions: [],
+      sugarList: [],
       milk:'',
-      milkOptions: [],
+      milkList: [],
       uid:'',
       userDisplayName:'',
-      userList:[]
+      userList:[],
+      userMap:{}
     };
   },
 
   componentWillMount: function() {
     var coffeeDataRef = this.props.model.firebaseRef.child('coffeeData');
-    coffeeDataRef.child('coffeeTypeList').on('value', this.onCoffeeTypeListValue);
-    coffeeDataRef.child('sugarTypeList').on('value', this.onSugarListValue);
-    coffeeDataRef.child('milkTypeList').on('value', this.onMilkListValue);
+    this.bindAsArray(coffeeDataRef.child('coffeeTypeList'), 'coffeeTypeList');
+    this.bindAsArray(coffeeDataRef.child('sugarTypeList'), 'sugarList');
+    this.bindAsArray(coffeeDataRef.child('milkTypeList'), 'milkList');
+    
     this.props.model.firebaseRef.child('users').on('value', this.onUserListValue);
     this.setUser(this.props.model.uid, this.props.model.userDisplayName);
   },
 
   componentWillUnmount: function() {
-    var coffeeDataRef = this.props.model.firebaseRef.child('coffeeData');
-    coffeeDataRef.child('coffeeTypeList').off('value', this.onCoffeeTypeListValue);
-    coffeeDataRef.child('sugarTypeList').off('value', this.onSugarListValue);
-    coffeeDataRef.child('milkTypeList').off('value', this.onMilkListValue);
     this.props.model.firebaseRef.child('users').off('value', this.onUserListValue);
   },
 
-  onCoffeeTypeListValue: function(snapshot) {
-    var options = [];
-    var value = snapshot.val();
-    for(var i in value){
-      var coffeeType = value[i];
-      options.push({ label: coffeeType.name,
-                     value: coffeeType.shortName });
-    }
-    this.setState({coffeeTypeOptions: options});
-  },
-
-  onCoffeeTypeChange: function(value){
-    var coffeeType = '';
-    if(value)
-      coffeeType = value.value;
+  onCoffeeTypeChange: function(event){
+    var coffeeType = event.target.value || '';
     this.setState({coffeeType: coffeeType});
     this.props.model.firebaseRef.child('userPreferences').child(this.state.uid).update({preferredCoffeeType: coffeeType});
   },
 
-  onSugarListValue: function(snapshot) {
-    var options = [];
-    var value = snapshot.val();
-    for(var i in value){
-      var sugar = value[i];
-      options.push({ label: sugar.name,
-                     value: sugar.quantity });
-    }
-    this.setState({sugarOptions: options});
-  },
-
   onSugarChange: function(value){
-    var sugar = 0;
-    if(value)
-      sugar = value.value;
+    var sugar = event.target.value || 0;
     this.setState({sugar: sugar});
     this.props.model.firebaseRef.child('userPreferences').child(this.state.uid).update({preferredSugar: sugar});
   },
 
-  onMilkListValue: function(snapshot) {
-    var options = [];
-    var value = snapshot.val();
-    for(var i in value){
-      var milk = value[i];
-      options.push({ label: milk.name,
-                     value: milk.name });
-    }
-    this.setState({milkOptions: options});
-  },
-
   onMilkChange: function(value){
-    var milk = '';
-    if(value)
-      milk = value.value;
+    var milk = event.target.value || '';
     this.setState({milk: milk});
     this.props.model.firebaseRef.child('userPreferences').child(this.state.uid).update({preferredMilk: milk});
   },
 
   onUserListValue: function(snapshot) {
     var users = [];
-    var userList = snapshot.val();
+    var userMap = snapshot.val();
     var options = [];
-    for(var i in userList){
-      options.push({ label: userList[i].displayName,
+    for(var i in userMap){
+      options.push({ label: userMap[i].displayName,
                      value: i });
     }
     options.sort(function(a, b) {
         return a.label < b.label ? -1 : 1;
       });
-    this.setState({userList: options});
+    this.setState({userList: options, userMap: userMap});
   },
 
   setUser: function(uid, userDisplayName){
@@ -127,9 +87,10 @@ var CoffeeOrder = React.createClass({
     }, this);
   },
 
-  onUserChange: function(value){
+  onUserChange: function(event){
+    var value = event.target.value;
     if(value)
-      this.setUser(value.value, value.label);
+      this.setUser(value, this.state.userMap[value].displayName);
     else
       this.setUser(this.props.model.uid, this.props.model.userDisplayName);
   },
@@ -152,37 +113,30 @@ var CoffeeOrder = React.createClass({
   },
 
   render: function() {
+    function createUserOption(item, index){
+      return <option key={index} value={item.value}>{item.label}</option>;
+    }
+    function createOption(item, index){
+      return <option key={index} value={item.shortName}>{item.name}</option>;
+    }
+    function createSugarOption(item, index){
+      return <option key={index} value={item.quantity}>{item.quantity}</option>;
+    }
     return (
-      <form onSubmit={ this.handleSubmit }>
-        <Select
-            name="userSelect"
-            value={this.state.uid}
-            options={this.state.userList}
-            placeholder="Select User - Defaults to Me"
-            onChange={this.onUserChange}
-        />
-        <Select
-            name="milkSelect"
-            value={this.state.milk}
-            options={this.state.milkOptions}
-            placeholder="Select Milk"
-            onChange={this.onMilkChange}
-        />
-        <Select
-            name="coffeeTypeSelect"
-            value={this.state.coffeeType}
-            options={this.state.coffeeTypeOptions}
-            placeholder="Select Coffee Type"
-            onChange={this.onCoffeeTypeChange}
-        />
-        <Select
-            name="sugarSelect"
-            value={this.state.sugar}
-            options={this.state.sugarOptions}
-            placeholder="Select Sugar"
-            onChange={this.onSugarChange}
-        />
-        <br className="clearBoth" />
+      <form onSubmit={ this.handleSubmit } className="form-inline">
+        <select className="form-control col-xs-10" value={this.state.uid} onChange={this.onUserChange}>
+          {this.state.userList.map(createUserOption)}
+        </select>
+        <select className="form-control col-sm-4" value={this.state.milk} onChange={this.onMilkChange}>
+          <option value=""></option>
+          {this.state.milkList.map(createOption)}
+        </select>
+        <select className="form-control col-sm-4" value={this.state.coffeeType} onChange={this.onCoffeeTypeChange}>
+          {this.state.coffeeTypeList.map(createOption)}
+        </select>
+        <select className="form-control col-sm-4" value={this.state.sugar} onChange={this.onSugarChange}>
+          {this.state.sugarList.map(createSugarOption)}
+        </select>
         <Button bsStyle="success" onClick={this.handleSubmit} >Order</Button>
       </form>
     );
