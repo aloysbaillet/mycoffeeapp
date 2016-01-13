7b0623f687dafe3fcb57db69fe571593e356af47
Aloys Baillet <aloys.baillet@gmail.com>
2016-01-13 21:44:58 +1100

    Fixed group selection

--- web/src/groupselect.js
+++ web/src/groupselect.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactFireMixin from 'reactfire';
-import Select from 'react-select';
-import {Input, Button} from 'react-bootstrap';
+import {Input, Button, Select} from 'react-bootstrap';
 
 var GroupSelect = React.createClass({
   mixins: [ReactFireMixin],
@@ -17,9 +16,9 @@ var GroupSelect = React.createClass({
 	this.bindAsArray(this.props.model.firebaseRef.child('userGroups').orderByChild('name'), 'groupList');
   },
   
-  onGroupSelect: function(option) {
-    var groupId = option.value;
-  	this.props.onGroupSelect(null);
+  onGroupSelect: function(event) {
+    var groupId = event.target.value;
+  	this.props.onGroupSelect(groupId);
   },
 
   onGroupNameChange: function(e) {
@@ -37,8 +36,8 @@ var GroupSelect = React.createClass({
 
   render: function() {
     var _this = this;
-    function createGroupItem(item, index){
-      return <li key={index} onClick={_this.onGroupSelect.bind(null, item['.key'])}><a href="#">{item.name}</a></li>
+    function createGroupOption(item, index){
+      return <option key={index} value={item['.key']}>{item.name}</option>
     }
     var groupList = [];
     for(var gid in this.state.groupList){
@@ -47,13 +46,9 @@ var GroupSelect = React.createClass({
     }
     return <div>
       <div className="input-group pull-left col-xs-5">
-        <Select
-          name="groupSelect"
-          value={this.props.model.groupId}
-          options={groupList}
-          placeholder="Select a Group"
-          onChange={this.onGroupSelect}
-          />
+        <select value={this.props.model.groupId || ""} className="form-control" onChange={this.onGroupSelect}>
+          {this.state.groupList.map(createGroupOption)}
+        </select>
       </div>
       <div className="input-group pull-right col-xs-4 input-sm">
         <Input type="text" placeholder="New Group" value={this.state.groupNameToAdd} onChange={this.onGroupNameChange}/>
